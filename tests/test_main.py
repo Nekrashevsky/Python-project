a25438a24235ac05b3d144451763da1804038604
Nekrashevsky <knekrashevskiy04@gmail.com>
2024-07-07 18:03:47 +0300

    Added project

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestCalculator(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(evaluate_expression("2 + 2"), 4)
+        self.assertEqual(evaluate_expression("10 + 5"), 15)
+        self.assertEqual(evaluate_expression("0 + 0"), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(evaluate_expression("5 - 3"), 2)
+        self.assertEqual(evaluate_expression("10 - 10"), 0)
+        self.assertEqual(evaluate_expression("0 - 5"), -5)
+
+    def test_invalid_expression(self):
+        self.assertIsInstance(evaluate_expression("2 +"), str)
+        self.assertIsInstance(evaluate_expression("5 -"), str)
+        self.assertIsInstance(evaluate_expression("invalid"), str)
+
+if __name__ == "__main__":
+    unittest.main()
